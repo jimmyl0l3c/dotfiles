a8ce8ecfb4b2696eb16e29c0c8442f154df79fb9
Filip Joska <filip@joska.dev>
2025-05-11 11:58:59 +0200

    Split workspaces module, show vertical workspaces on second monitor

--- dot_config/eww/modules/workspaces/scripts/get_active_vertical.sh
+++ dot_config/eww/modules/workspaces/scripts/get_active_vertical.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+hyprctl monitors -j | jq -c 'first(.[] | select(.model == "LF24T370F") | .activeWorkspace.id)'
 
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
   stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ && $2 >= 6 && $2 <= 9 {print $2}'
